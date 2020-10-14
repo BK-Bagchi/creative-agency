2e2fe0181a85c2c2bd44fd7d1606933a59858fea
my name <you@exampl.com>
2020-10-14 16:12:54 +0600

    project setup initialized

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <section>
+
+            </section>
+        </>
+    );
+};
+
+export default Header;
