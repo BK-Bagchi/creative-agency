e7d05327b940db6e2dbb842a7ab863bbf664cb8a
my name <you@exampl.com>
2020-10-16 00:50:15 +0600

    adminServiceList page static markup done

--- src/Components/Dashboard/AdminServiceList/AdminServiceList.js
+++ src/Components/Dashboard/AdminServiceList/AdminServiceList.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React from 'react'
+import '../DHome.css'
 
 const AdminServiceList = () => {
     return (
-        <div>
+        <section className="admin-service-list">
             AdminServiceList
-        </div>
+            <table className="w-100 text-center">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email ID</th>
+                        <th>Service</th>
+                        <th>Project Details</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email ID</td>
+                        <td>Service</td>
+                        <td>Project Details</td>
+                        <td>
+                            <select name="" value="" onChange={""}>
+                                <option value="pending">Pending</option>
+                                <option value="done">Done</option>
+                                <option value="ongoing">On Going</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email ID</td>
+                        <td>Service</td>
+                        <td>Project Details</td>
+                        <td>
+                            <select name="" value="" onChange={""}>
+                                <option value="pending">Pending</option>
+                                <option value="done">Done</option>
+                                <option value="ongoing">On Going</option>
+                            </select>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     );
 };
 
