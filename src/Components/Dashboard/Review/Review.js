9518dde33a592cfac109110eabc220ccab21f29a
my name <you@exampl.com>
2020-10-16 21:09:56 +0600

    user can give a review

--- src/Components/Dashboard/Review/Review.js
+++ src/Components/Dashboard/Review/Review.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Review = () => {
+    const loginInfo = JSON.parse(sessionStorage.getItem('loginInfo'))
+    const [formInfo, setFormInfo] = useState({
+        name: loginInfo.displayName,
+        email: loginInfo.email,
+        description: ''
+    });
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const collectFormInfo = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+
+        if (name === 'description') {
+            setFormInfo({ ...formInfo, description: value })
+        }
+    }
+    const submitForm = (e) => {
+        e.preventDefault()
+        const { description } = formInfo
+        if (!description) {
+            setErrorMessage('Please insert a description')
+            return
+        }
+        fetch('http://localhost:4000/userReview', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formInfo)
+        })
+            .then(res => res.json())
+            .then(data => alert(`${data.insertedCount} review inserted`))
+            .catch(err => {
+                alert('Oops!! Something went wrong during inserting data')
+                console.log(err)
+            })
+    }
+
     return (
         <section className="review">
-            <form className="d-flex flex-column">
-                <input type="text" placeholder="Your name/company's name" />
-                <input type="email" placeholder="Your email address" />
-                <textarea rows="10" placeholder="Your Description"></textarea>
+            <form className="d-flex flex-column" onSubmit={submitForm}>
+                <input name="name" type="text" placeholder="Your name/company's name" value={loginInfo.displayName} readOnly />
+                <input name="email" type="email" placeholder="Your email address" value={loginInfo.email} readOnly />
+                <textarea name="description" rows="10" placeholder="Your Description" onBlur={collectFormInfo}></textarea>
+
+                <span style={{ color: 'red' }} className="error">{errorMessage}</span>
                 <input type="submit" value="Send" />
             </form>
         </section>
--- src/Components/Dashboard/UserServiceList/UserServiceList.js
+++ src/Components/Dashboard/UserServiceList/UserServiceList.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Img from '../../../Resources/images/icons/service1.png'
 
 const UserServiceList = () => {
+    const loginInfo = JSON.parse(sessionStorage.getItem('loginInfo'))
+    const [orderedList, setOrderedList] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetch('http://localhost:4000/showOrderedService', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: loginInfo.email })
+        })
+            .then(res => res.json())
+            .then(data => {
+                setOrderedList(data)
+                setLoading(false)
+            })
+    }, [])
+
     return (
         <section className="user-service-list">
             <div className="row">
-                <div className="col-md-6">
-                    <div className="px-3 service-item">
-                        <div className="d-flex justify-content-between">
-                            <img style={{ width: '70px' }} src={Img} alt="Img" />
-                            <button>Done</button>
+                {
+                    loading ?
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
                         </div>
-                        <h3>Web & Mobile design</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio, soluta ullam commodi maxime dolorum?</p>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="px-3 service-item">
-                        <div className="d-flex justify-content-between">
-                            <img style={{ width: '70px' }} src={Img} alt="Img" />
-                            <button>Done</button>
-                        </div>
-                        <h3>Web & Mobile design</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio, soluta ullam commodi maxime dolorum?</p>
-                    </div>
-                </div>
+                        :
+                        <>
+                            {
+                                orderedList.map(item => {
+                                    const { _id, status, order, description } = item
+                                    return (
+                                        <div key={_id} className="col-md-6">
+                                            <div className="px-3 service-item">
+                                                <div className="d-flex justify-content-between">
+                                                    <img style={{ width: '70px' }} src={Img} alt="Img" />
+                                                    <button>{status}</button>
+                                                </div>
+                                                <h3>{order}</h3>
+                                                <p>{description}</p>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </>
+                }
             </div>
         </section>
     );
