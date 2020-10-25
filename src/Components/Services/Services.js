10074c2de097ab661bc7c1cb82d7668b818ab939
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-10-25 14:03:45 +0600

    onClick on Service list, redirects to dashboard

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -3,10 +3,11 @@ import './Services.css'
 import Service1 from '../../Resources/images/icons/service1.png'
 import Service2 from '../../Resources/images/icons/service2.png'
 import Service3 from '../../Resources/images/icons/service3.png'
+import { useHistory } from 'react-router-dom'
 
-const Service = ({ image, service }) => {
+const Service = ({ image, service, history }) => {
     return (
-        <div className="col-md-4 service d-flex flex-column">
+        <div className="col-md-4 service d-flex flex-column" onClick={() => history.push('/dashboard')}>
             <img className="service-img" src={image} alt="Service" />
             <h5 className="text-center font-weight-bold">{service}</h5>
             <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolore quas facilis dolores cupiditate optio?</p>
@@ -16,6 +17,8 @@ const Service = ({ image, service }) => {
 const Services = () => {
     const [services, setServices] = useState([])
     const [loading, setLoading] = useState(true)
+    const history = useHistory()
+
     useEffect(() => {
         fetch('https://safe-brushlands-61338.herokuapp.com/services')
             .then(res => res.json())
@@ -35,14 +38,14 @@ const Services = () => {
                                 <span className="sr-only">Loading...</span>
                             </div> :
                             <>
-                                <Service image={Service1} service={'Web and Mobile Design'} />
-                                <Service image={Service2} service={'Graphic Design'} />
-                                <Service image={Service3} service={'Webs Development'} />
+                                <Service image={Service1} service={'Web and Mobile Design'} history={history} />
+                                <Service image={Service2} service={'Graphic Design'} history={history} />
+                                <Service image={Service3} service={'Webs Development'} history={history} />
                                 {
                                     services.map(service => {
                                         const { _id, serviceName, description } = service
                                         return (
-                                            <div key={_id} className="col-md-4 service d-flex flex-column">
+                                            <div key={_id} className="col-md-4 service d-flex flex-column" onClick={() => history.push('/dashboard')}>
                                                 <img className="service-img" src={Service3} alt="Service" />
                                                 <h5 className="text-center font-weight-bold">{serviceName}</h5>
                                                 <p className="text-center">{description}</p>
